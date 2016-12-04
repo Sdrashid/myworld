b97f0f9b7c94e41b4bd4d35700cb16a6d6983e58
sdrashid <sayyed.rashid2003@gmail.com>
2016-12-04 14:14:19 +0530

    Dev Update

--- GitTest/src/gittest/GitTest.java
+++ GitTest/src/gittest/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 		// TODO Auto-generated method stub
 		System.out.println("This is GIT Test...");
 		System.out.println("This is GIT Test...File Update.");
-		System.out.println("This is GIT Test...Branch.");
+		System.out.println("This is GIT Dev...Branch.");
 	}
 
 }
